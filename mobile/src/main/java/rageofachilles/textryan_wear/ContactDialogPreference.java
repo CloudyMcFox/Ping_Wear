c8f2374b897e36b452778729a3fa22c96a8764de
Ryan Offir <Ryan Offir>
2016-05-17 22:57:17 -0700

    Fix ocontacts layout and fix perf issue on getting contacts

--- mobile/src/main/java/rageofachilles/textryan_wear/ContactDialogPreference.java
+++ mobile/src/main/java/rageofachilles/textryan_wear/ContactDialogPreference.java
@@ -3,7 +3,6 @@ package rageofachilles.textryan_wear;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.preference.DialogPreference;
@@ -16,32 +15,20 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * Created by Ryan on 5/15/2016.
  */
 public class ContactDialogPreference extends DialogPreference
 {
-
-
-
-    private Uri QUERY_URI = ContactsContract.Contacts.CONTENT_URI;
+    private Uri QUERY_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
     private String CONTACT_ID = ContactsContract.Contacts._ID;
     private String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
-    private Uri EMAIL_CONTENT_URI = ContactsContract.CommonDataKinds.Email.CONTENT_URI;
-    private String EMAIL_CONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
-    private String EMAIL_DATA = ContactsContract.CommonDataKinds.Email.DATA;
     private String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
     private String PHONE_NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
-    private Uri PHONE_CONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-    private String PHONE_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
-    private String STARRED_CONTACT = ContactsContract.Contacts.STARRED;
-
-
-
+    private int MAX_STRING = 20;
 
     public ContactDialogPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
@@ -68,82 +55,65 @@ public class ContactDialogPreference extends DialogPreference
 
     }
 
-
     public ArrayList<Contact> getContacts()
     {
-        ArrayList<Contact> contactList = new ArrayList<>();
-
-        //String selection = ContactsContract.Contacts.DISPLAY_NAME ;//+ " LIKE ? and " + ContactsContract.Contacts.HAS_PHONE_NUMBER + " > 0 ";
         ContentResolver cr = getContext().getContentResolver();
 
-        String[] projection = new String[]{CONTACT_ID, DISPLAY_NAME, HAS_PHONE_NUMBER, STARRED_CONTACT};
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-                projection, null, null, null);
+        String[] projection = new String[]{CONTACT_ID, DISPLAY_NAME, HAS_PHONE_NUMBER, Phone.TYPE, PHONE_NUMBER};
 
-//        if (cur.getCount() > 0) {
-//                    while (cur.moveToNext()) {
-//                        if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0){
-//                    String name = cur.getString(cur.getColumnIndex(
-//                            ContactsContract.Contacts.DISPLAY_NAME));
-//
-//                    String contactId = cur.getString(cur.getColumnIndex(CONTACT_ID));
-//                    Cursor phoneCursor = cr2.query(ContactsContract.Contacts.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{contactId}, null);
-//                    while (phoneCursor.moveToNext()) {
-//                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(PHONE_NUMBER));
-//                        String number = phoneNumber;
-//                        contactList.add(new Contact(name, number));
-//                    }
-//
-//                    phoneCursor.close();
-//                }
-//            }
-//            cur.close();
-//        }
+        Cursor cursor = cr.query(QUERY_URI, projection, ContactsContract.Contacts.HAS_PHONE_NUMBER, null,  ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
 
-        Cursor cursor = cr.query(QUERY_URI, projection, null, null,  ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC");
-
-        while (cursor.moveToNext()) {
-            ArrayList<Contact> contacts = getContact(cursor);
-            for (Contact cont : contacts) {
-                contactList.add(cont);
-            }
-        }
-
-        cursor.close();
-        return contactList;
-    }
-
-    private ArrayList<Contact> getContact(Cursor cursor) {
         ArrayList<Contact> contacts = new ArrayList<>();
-        String contactId = cursor.getString(cursor.getColumnIndex(CONTACT_ID));
-        String name = (cursor.getString(cursor.getColumnIndex(DISPLAY_NAME)));
-        Uri uri = Uri.withAppendedPath(QUERY_URI, String.valueOf(contactId));
 
-        ArrayList<String> numbers = getPhone(cursor, contactId);
-        for(String num : numbers) {
-            contacts.add(new Contact(name, num, contactId));
+        String oldName = "";
+        int count =0;
+        while (cursor.moveToNext()) {
+
+            String name = (cursor.getString(cursor.getColumnIndex(DISPLAY_NAME))).trim();
+            final int typeIndex = cursor.getColumnIndex(Phone.TYPE);
+            final int numberIndex = cursor.getColumnIndex(PHONE_NUMBER);
+
+            int type = cursor.getInt(typeIndex);
+            String phoneNumber = cursor.getString(numberIndex);
+
+            if(name.equals(oldName)) {
+                oldName = name;
+                name = ""; // Don't display multiple names for same contact
+            } else {
+                oldName = name;
+            }
+            contacts.add(new Contact(name, phoneNumber, TypeToString(type) ));
+            count++;
         }
-
+        Log.v("myTag",""+count);
+        cursor.close();
         return contacts;
     }
 
-    private ArrayList<String> getPhone(Cursor cursor, String contactId) {
-        ArrayList<String> numbers = new ArrayList<>();
-        ContentResolver contentResolver = getContext().getContentResolver();
-        int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
-        Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
-        if (hasPhoneNumber > 0) {
-            Cursor phoneCursor = contentResolver.query(PHONE_CONTENT_URI, null, PHONE_CONTACT_ID + " = ?", new String[]{contactId}, null);
-            while (phoneCursor.moveToNext()) {
-                int type = phoneCursor.getInt(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
-                if (type == ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE || type == ContactsContract.CommonDataKinds.Phone.TYPE_WORK_MOBILE) {
-                    String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(PHONE_NUMBER));
-                    numbers.add(phoneNumber);
-                }
-            }
-            phoneCursor.close();
+    protected String TypeToString(int type)
+    {
+        switch (type) {
+            case Phone.TYPE_HOME:
+                return "Home";
+            case Phone.TYPE_MOBILE:
+                return "Mobile";
+            case Phone.TYPE_WORK:
+                return "Work";
+            case Phone.TYPE_FAX_HOME:
+                return "Home Fax";
+            case Phone.TYPE_FAX_WORK:
+                return "Work Fax";
+            case Phone.TYPE_MAIN:
+                return "Main";
+            case Phone.TYPE_OTHER:
+                return "Other";
+            case Phone.TYPE_CUSTOM:
+                return "Custom";
+            case Phone.TYPE_PAGER:
+                return "Pager";
+            default:
+                return "";
         }
-        return numbers;
     }
 
     @Override
@@ -160,9 +130,6 @@ public class ContactDialogPreference extends DialogPreference
 
         super.onBindDialogView(view);
     }
-
-
-
 
     public class ContactAdapter extends ArrayAdapter<Contact>
     {
@@ -182,27 +149,29 @@ public class ContactDialogPreference extends DialogPreference
             View row = convertView;
             ViewHolder holder;
 
-            if(row == null)
-            {
+            if(row == null) {
                 LayoutInflater inflater = ((Activity)context).getLayoutInflater();
                 row = inflater.inflate(layoutResourceId, parent, false);
 
                 holder = new ViewHolder();
                 holder.textView1 = (TextView)row.findViewById(R.id.lblName);
-                holder.textView2 = (TextView)row.findViewById(R.id.lblNumber);
-
+                holder.textView2 = (TextView)row.findViewById(R.id.lblType);
+                holder.textView3 = (TextView)row.findViewById(R.id.lblNumber);
                 row.setTag(holder);
-            }
-            else
-            {
+            } else {
                 holder = (ViewHolder)row.getTag();
             }
 
             Contact person = data.get(position);
 
-            holder.textView1.setText(person.getName());
-            holder.textView2.setText(person.getNumber());
-
+            String name = person.getName();
+            if (name.length() > MAX_STRING) {
+                name = name.substring(0, MAX_STRING);
+                name = name.concat("...");
+            }
+            holder.textView1.setText(name);
+            holder.textView2.setText(person.getType());
+            holder.textView3.setText(person.getNumber());
             return row;
         }
 
@@ -210,22 +179,20 @@ public class ContactDialogPreference extends DialogPreference
         {
             TextView textView1;
             TextView textView2;
-
+            TextView textView3;
         }
     }
-
 
     public class Contact
     {
         String name;
         String number;
-        String id;
+        String type;
         // Picture
-        public Contact (String input_name, String input_number, String input_id)
-        {
+        public Contact (String input_name, String input_number, String input_type) {
             name = input_name;
             number = input_number;
-            id = input_id;
+            type = input_type;
         }
 
         public String getName(){
@@ -234,6 +201,10 @@ public class ContactDialogPreference extends DialogPreference
 
         public String getNumber(){
             return number;
+        }
+
+        public String getType(){
+            return type;
         }
     }
 }
